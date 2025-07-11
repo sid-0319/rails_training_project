b7c1f3c5affd954a86f1589f85dbdabeaf325888
Siddhi Rathod <siddhi.rathod@vtinfo.com>
2025-07-11 06:55:08 +0000

    Added test cases

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -20,4 +20,13 @@ RSpec.feature "User Sign In", type: :feature do
 
     expect(page).to have_content("Invalid Email or password")
   end
+
+  scenario "email and password cannot be empty" do
+    visit new_user_session_path
+    fill_in "Email", with: ""
+    fill_in "Password", with: ""
+    click_button "Log in"
+
+    expect(page).to have_content("Invalid Email or password")
+  end
 end
