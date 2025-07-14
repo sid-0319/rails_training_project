3fe9475d279904c015342344e52d71e27bc70b1c
Siddhi Rathod <siddhi.rathod@vtinfo.com>
2025-07-14 04:48:03 +0000

    Some changes

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -3,8 +3,8 @@ FactoryBot.define do
     first_name { Faker::Name.first_name }
     last_name  { Faker::Name.last_name }
     email { Faker::Internet.unique.email }
-    age { rand(18..60) }
-    date_of_birth { Faker::Date.birthday(min_age: 18, max_age: 60) }
+    age { rand(1..99) }
+    date_of_birth { Faker::Date.birthday(min_age: 1, max_age: 99) }
     phone_number { Faker::PhoneNumber.cell_phone }
     password { "Password@123" }
     password_confirmation { "Password@123" }
