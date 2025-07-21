678bdcdd8380618c21450233b5589aa458ba4e89
Siddhi Rathod <siddhi.rathod@vtinfo.com>
2025-07-21 09:51:31 +0000

    made changes

--- app/models/token.rb
+++ app/models/token.rb
@@ -4,7 +4,7 @@ class Token < ApplicationRecord
   private
 
   def generate_value_and_expiration
-    self.value = SecureRandom.hex(20)
+    self.value = SecureRandom.hex(32)
     self.expired_at = 24.hours.from_now
   end
 end
