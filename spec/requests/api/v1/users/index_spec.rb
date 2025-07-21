ed168aaa87bd79d8024ab44a0f6fecf6dbc37893
Siddhi Rathod <siddhi.rathod@vtinfo.com>
2025-07-21 09:34:53 +0000

    Made changes

--- spec/requests/api/v1/users/index_spec.rb
+++ spec/requests/api/v1/users/index_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe 'API::V1::Users', type: :request do
   describe 'GET /api/v1/users' do
+    let(:json_response) { JSON.parse(response.body) }
+
     before do
       create_list(:user, 5)
       get '/api/v1/users'
@@ -12,20 +14,17 @@ RSpec.describe 'API::V1::Users', type: :request do
     end
 
     it 'returns all users with required fields only' do
-      json_response = JSON.parse(response.body)
       expect(json_response.length).to eq(5)
       expect(json_response.first.keys).to match_array(%w[id first_name last_name email created_at])
     end
 
     it 'does not expose sensitive fields like password or encrypted_password' do
-      json_response = JSON.parse(response.body)
       user_keys = json_response.first.keys
       expect(user_keys).not_to include('password')
       expect(user_keys).not_to include('encrypted_password')
     end
 
     it 'returns valid data types for each field' do
-      json_response = JSON.parse(response.body)
       user = json_response.first
       expect(user['id']).to be_an(Integer)
       expect(user['first_name']).to be_a(String)
@@ -35,13 +34,11 @@ RSpec.describe 'API::V1::Users', type: :request do
     end
 
     it 'returns users ordered by creation time ascending (oldest first)' do
-      json_response = JSON.parse(response.body)
       creation_times = json_response.map { |u| Time.parse(u['created_at']) }
       expect(creation_times).to eq(creation_times.sort)
     end
 
     it 'returns unique emails for each user' do
-      json_response = JSON.parse(response.body)
       emails = json_response.map { |u| u['email'] }
       expect(emails.uniq.length).to eq(emails.length)
     end
